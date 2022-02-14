338a2915be48baa56e34edbce6ff75aa3a6b876a
Nils Olsson <nilso@enosis.net>
2022-02-14 11:21:59 -0800

    Lazy commit

--- config/nvim/lua/plugins/nvim-lint.lua
+++ config/nvim/lua/plugins/nvim-lint.lua
@@ -1,13 +1,22 @@
-local util = require('util')
+local util = require("util")
 local map = util.map
-
-require('lint').linters_by_ft = {
+require("lint").linters_by_ft = {
     python = {
-        'mypy',
-        'pycodestyle',
-        'pydocstyle',
-        'flake8',
+        "mypy",
+        "pycodestyle",
+        "pydocstyle",
+        "flake8",
     },
+}
+-- TODO: Only run this within Python contexts
+require("lint.linters.mypy").args = {
+    "--show-column-numbers",
+    "--hide-error-codes",
+    "--hide-error-context",
+    "--no-color-output",
+    "--no-error-summary",
+    "--no-pretty",
+    "--strict",
 }
 
 local opts = { noremap = false }
@@ -18,6 +27,6 @@ map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
 vim.api.nvim_exec([[
 augroup PythonLint
     autocmd!
-    autocmd BufWritePost <buffer> lua require('lint').try_lint()
+    autocmd BufWritePost <buffer> lua require("lint").try_lint()
 augroup end
 ]], false)
