6a52198bde3d2ac24aa315efbc919e7de14e4579
Nils Olsson <nilso@enosis.net>
2022-02-22 17:03:15 -0800

    Lazy commit

--- config/nvim/lua/plugins/LuaSnip.lua
+++ config/nvim/lua/plugins/LuaSnip.lua
@@ -9,6 +9,8 @@ local c = ls.choice_node
 local d = ls.dynamic_node
 local r = ls.restore_node
 local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local fmt = require("luasnip.extras.fmt").fmt
 
 local types = require("luasnip.util.types")
 
@@ -27,19 +29,47 @@ ls.config.set_config({
     enable_autosnippets = false,
 })
 
-ls.snippets = {
-    all = {
-    },
-    python = {
-    },
-    rust = {
-    },
-    lua = {
-        ls.parser.parse_snippet("expand", "-- hello"),
-    },
+local H = "html"
+local M = "markdown"
+-- local P = "python"
+
+local snippet_groups = {
+    {
+        {H,M}, {
+            s(
+                "tag",
+                fmt("<{}{}>{}</{}>", {
+                    i(1),
+                    i(2),
+                    i(3),
+                    rep(1),
+                })
+            )
+        }
+    }
 }
+
+for _, pair in pairs(snippet_groups) do
+    local langs = pair[1]
+    local snippets = pair[2]
+    for _, lang in pairs(langs) do
+        for _, snippet in pairs(snippets) do
+            ls.snippets[lang] = ls.snippets[lang] or {}
+            table.insert(ls.snippets[lang], snippet)
+        end
+    end
+    -- ls.snippets[lang] = {}
+end
+
+-- ls.snippets = {
+--     all = {},
+--     python = {},
+--     rust = {},
+--     lua = {},
+-- }
 
 -- vim.keymap.set({"i", "s"}, "<c-k>", function()
 -- end, {silent = true})
 
 -- vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/lua/plugins/LuaSnip.lua<CR>")
+vim.keymap.set("n", "<leader>ls", "<cmd>source ~/.config/nvim/lua/plugins/LuaSnip.lua<CR>")
